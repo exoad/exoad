44d565cf23bc9c7d0d48c8800a2556e8abd62c8e
jackm <jackmeng0814@gmail.com>
2024-12-14 02:22:34 +0800

    Update and rename README.txt to README.java

--- README.java
+++ README.java
@@ -1,6 +1,8 @@
+"""
 i love demanding hyper-pressurized 
 crystalized sand pancake to wiggle its 
 electrons in a specific patterns                           http://www.mit.edu/people/dpolicar/writing/prose/text/thinkingMeat.html
+"""
 
-
-
+what i use:
+  
